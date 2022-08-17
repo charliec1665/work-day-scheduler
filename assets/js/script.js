f72a59af867d56953ac6df117bcf6ffbbcfd49c0
Charlie Carter <charliec1665@gmail.com>
2022-08-17 14:48:50 -0400

    current date displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
 var displayDateEl = document.querySelector("#currentDay");
+var timeBlocksEl = document.querySelector(".container");
 
 // display current day at top of planner using moment.js
 var displayCurrentDate = function() {
     // create variable to show live date using moment.js
-    var date = moment().format("MMM Do YY");
-    console.log(date);
-    // assign text content to element
-    var dateEl = displayDateEl.textContent(date);
-    // append date to container
-    displayDateEl.appendChild(dateEl);
+    var dateEl = moment().format("MMM Do YY");
+    // set date as containers text content
+    displayDateEl.textContent = dateEl
 }
 
 // create time blocks 9-5 with save btn using bootstrap
+
     // color code time blocks with .past, .present, .future
     // time blocks using moment.js to sync with live time
 // add click event listeners so that the user can enter tasks in time blocks
